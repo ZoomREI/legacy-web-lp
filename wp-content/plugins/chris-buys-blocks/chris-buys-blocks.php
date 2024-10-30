97c04e51e39622388264441dbeb14bbed92dc786
ZoomREI <dev@zoomrei.com>
2024-10-30 16:38:54 +0200

    Added Wordfence, Added OptinMonster, updated plugins, Added block CSS filter to load only used block styles

--- wp-content/plugins/chris-buys-blocks/chris-buys-blocks.php
+++ wp-content/plugins/chris-buys-blocks/chris-buys-blocks.php
@@ -93,3 +93,5 @@ function chris_buys_blocks_chris_buys_blocks_block_init()
 	}
 }
 add_action('init', 'chris_buys_blocks_chris_buys_blocks_block_init');
+
+add_filter('should_load_separate_core_block_assets', '__return_true');
